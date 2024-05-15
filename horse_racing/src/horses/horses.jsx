568a406db6b904704a62714174049385f673d154
stakiv <125588447+stakiv@users.noreply.github.com>
2024-05-15 23:55:24 +0300

    updated content in horses & jockeys

--- horse_racing/src/horses/horses.jsx
+++ horse_racing/src/horses/horses.jsx
@@ -26,14 +26,66 @@ const Main = () => {
             <div className={j.sort}>
                 Сортировать по
                 <div className={j.option}>
-                    <input type='button' value={"Количество побед"} />
+                    <input type='button' value={"Количество побед"}  className={j.date}/>
                 </div>
             </div>
             <div className={j.items}>
                 <table className={j.item}>
 
                     <tr className={j.row}>
-                        <th className={j.data}>№</th>
+                        <th className={j.data}>ID</th>
+                        <th className={j.data}>Кличка</th>
+                        <th className={j.data}>Масть</th>
+                        <th className={j.data}>Возраст</th>
+                        <th className={j.data}>Владелец</th>
+                        <th className={j.data}>Кол-во побед</th>
+                    </tr>
+                    
+                        {horses.map(i =>
+                            <tr className={j.row}>
+                                <td className={j.data}>{i.id}</td>
+                                <td className={j.data}>{i.name}</td>
+                                <td className={j.data}>{i.suit}</td>
+                                <td className={j.data}>{i.age}</td>
+                                <td className={j.data}>{i.owner}</td>
+                                <td className={j.data}>{i.wins}</td>
+                            </tr>
+
+                        )}
+                    
+                </table>
+            </div>
+            <div className={j.items}>
+                <table className={j.item}>
+
+                    <tr className={j.row}>
+                        <th className={j.data}>ID</th>
+                        <th className={j.data}>Кличка</th>
+                        <th className={j.data}>Масть</th>
+                        <th className={j.data}>Возраст</th>
+                        <th className={j.data}>Владелец</th>
+                        <th className={j.data}>Кол-во побед</th>
+                    </tr>
+                    
+                        {horses.map(i =>
+                            <tr className={j.row}>
+                                <td className={j.data}>{i.id}</td>
+                                <td className={j.data}>{i.name}</td>
+                                <td className={j.data}>{i.suit}</td>
+                                <td className={j.data}>{i.age}</td>
+                                <td className={j.data}>{i.owner}</td>
+                                <td className={j.data}>{i.wins}</td>
+                            </tr>
+
+                        )}
+                    
+                </table>
+            </div>
+            <div className={j.items}>
+                <table className={j.item}>
+
+                    <tr className={j.row}>
+                        <th className={j.data}>ID</th>
                         <th className={j.data}>Кличка</th>
                         <th className={j.data}>Масть</th>
                         <th className={j.data}>Возраст</th>
--- horse_racing/src/jockeys/jockeys.jsx
+++ horse_racing/src/jockeys/jockeys.jsx
@@ -1,15 +1,58 @@
-
+import j from './jockeys.module.css'
 
 const jockeys = [
-    "Рябова Елизавета", "Леонова Екатерина", "Рябова Екатерина", "Екатерина Рябова"
+    {
+        id: 0,
+        name: "Рябова Елизавета",
+        age: 3,
+        wins: 8,
+    },
+    {
+        id: 1,
+        name: "Леонова Екатерина",
+        age: 3,
+        wins: 8,
+    },
+    {
+        id: 2,
+        name: "Рябова Екатерина",
+        age: 3,
+        wins: 8,
+    }
 ]
 
 const Main = () => {
     return (
+        <main className={j.main}>
+            <div className={j.sort}>
+                Сортировать по
+                <div className={j.option}>
+                    <input type='button' value={"Количество побед"}  className={j.date}/>
+                </div>
+            </div>
+            <div className={j.items}>
+                <table className={j.item}>
 
-        <main >
-            ЖЖЖЖОООООККККЕЕЕЕЕЕИИИИИИ
-            
+                    <tr className={j.row}>
+                        <th className={j.data}>ID</th>
+                        <th className={j.data}>ФИО</th>
+                        <th className={j.data}>Возраст</th>
+                        <th className={j.data}>Кол-во побед</th>
+                    </tr>
+                    
+                        {jockeys.map(i =>
+                            <tr className={j.row}>
+                                <td className={j.data}>{i.id}</td>
+                                <td className={j.data}>{i.name}</td>
+                                <td className={j.data}>{i.age}</td>
+                                <td className={j.data}>{i.wins}</td>
+                            </tr>
+
+                        )}
+                    
+                </table>
+            </div>
+
         </main>
     )
 }
