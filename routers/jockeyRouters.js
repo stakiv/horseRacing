68c7025b324a3a0bc73759080f1400495f9d56aa
stakiv <125588447+stakiv@users.noreply.github.com>
2024-05-25 00:48:43 +0300

    Create jockeyRouters.js

---
+++ routers/jockeyRouters.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const jockeyController = require("../controllers/jockeys");
+const jockeyRouter = express.Router();
+
+jockeyRouter.use("/races", jockeyController.find_jockeys);
+
+module.exports = raceRouter;
