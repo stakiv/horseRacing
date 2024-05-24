ccf0772f2d35f440bfa297886b676242202e0ab9
stakiv <125588447+stakiv@users.noreply.github.com>
2024-05-25 00:46:28 +0300

    raceRouters.js

---
+++ routers/racesRouters.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const raceController = require("../controllers/races");
+const raceRouter = express.Router();
+
+raceRouter.use("/races", raceController.find_races_date);
+raceRouter.use("/races", raceController.find_races_horse);
+raceRouter.use("/races", raceController.find_races_jockey);
+
+module.exports = raceRouter;
