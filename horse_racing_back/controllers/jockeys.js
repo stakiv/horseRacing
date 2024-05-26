edd96f66e4d02266945c1cdfe9a83c8b8fc56e02
stakiv <125588447+stakiv@users.noreply.github.com>
2024-05-26 22:39:51 +0300

    update controllers

---
+++ horse_racing_back/controllers/jockeys.js
@@ -0,0 +1,60 @@
+const Pool = require('pg').Pool
+const express = require("express")
+const pool = new Pool({
+    user: 'postgres',
+    password: 'mirandolina',
+    host: 'localhost',
+    port: 5432,
+    database: 'horse_racing'
+});
+const app = express();
+app.use((req, res, next) => {
+    res.header('Access-control-Allow-Origin', '*');
+    res.header('Access-control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-control-Allow-Headers', 'Content-Type, Authorization');
+    next()
+});
+
+exports.find_jockeys = app.get("", async(req, res) => {
+    try {
+        const sort = req.query.order;
+        if (sort == '') {
+            const Jockey = await pool.query(
+                `SELECT "winners".jockey_id, jockey_name, jockey_age, "wins" FROM (
+                    SELECT jockey_id, COUNT(*) AS "wins" FROM winners
+                    JOIN participants ON winners.participant_id = participants.participant_id
+                    GROUP BY jockey_id
+                    ) AS "winners"
+                    JOIN jockeys ON jockeys.jockey_id = "winners".jockey_id
+                    ORDER BY jockey_name ASC`
+            )
+            result = Jockey['rows']
+        } else {
+            const Jockey = await pool.query(
+                `SELECT "winners".jockey_id, jockey_name, jockey_age, "wins" FROM (
+                    SELECT jockey_id, COUNT(*) AS "wins" FROM winners
+                    JOIN participants ON winners.participant_id = participants.participant_id
+                    GROUP BY jockey_id
+                    ) AS "winners"
+                    JOIN jockeys ON jockeys.jockey_id = "winners".jockey_id
+                    ORDER BY "wins" ${sort}`
+            )
+            result = Jockey['rows']
+        }
+        /*
+        const Jockey = await pool.query(
+            `SELECT "winners".jockey_id, jockey_name, jockey_age, "wins" FROM (
+                SELECT jockey_id, COUNT(*) AS "wins" FROM winners
+                JOIN participants ON winners.participant_id = participants.participant_id
+                GROUP BY jockey_id
+                ) AS "winners"
+                JOIN jockeys ON jockeys.jockey_id = "winners".jockey_id
+				WHERE jockey_name = '${filter}'`
+        )*/
+        res.json(result)
+    }
+    catch (err) {
+        res.status(400).json({message: ""});
+        console.error(err)
+    }
+});
