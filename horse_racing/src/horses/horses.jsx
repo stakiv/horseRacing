c376d634ef6eba65bdfa46523f34b1396a5f5dd1
stakiv <125588447+stakiv@users.noreply.github.com>
2024-05-29 17:11:45 +0300

    update front & back

--- horse_racing/src/horses/horses.jsx
+++ horse_racing/src/horses/horses.jsx
@@ -22,43 +22,64 @@ const horses = [
 ]*/
 
 const Main = () => {
+    const location = useLocation();
+    const urlParams = new URLSearchParams(location.search);
+    const orderby = urlParams.get('orderby');
+
     const [horses, setHorses] = useState([]);
+    const [resList, SetResList] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
-        fetch('http://localhost:1337/api/horses').then((res) => res.json()).then((res) => {setHorses(res)})}, []);
+        const getHorses = async () => {
+            const res = await fetch('http://localhost:1337/api/horses?order=' + orderby, {
+                method: "GET"
+            });
+            if (res.ok) {
+                const data = await res.json()
+                SetResList(data);
+                setIsLoading(false)
+            }
+        }
+        getHorses();
+    })
+    if (isLoading) {
+        return <div>Загрузка...</div>
+    }
     return (
         <main className={j.main}>
             <div className={j.sort}>
                 Сортировать по
                 <div className={j.option}>
-                    <input type='button' value={"Количество побед"}  className={j.date}/>
+                    <input type='button' value={"Количество побед"} className={j.date} onClick={handleOrder}/>
                 </div>
             </div>
             <div className={j.items}>
                 <div className={j.marg}>
-                <table className={j.item}>
+                    <table className={j.item}>
 
-                    <tr className={j.row}>
-                        <th className={j.data}>ID</th>
-                        <th className={j.data}>Кличка</th>
-                        <th className={j.data}>Масть</th>
-                        <th className={j.data}>Возраст</th>
-                        <th className={j.data}>Владелец</th>
-                        <th className={j.data}>Кол-во побед</th>
-                    </tr>
-                    
-                        {horses.map(i =>
+                        <tr className={j.row}>
+                            <th className={j.data}>ID</th>
+                            <th className={j.data}>Кличка</th>
+                            <th className={j.data}>Масть</th>
+                            <th className={j.data}>Возраст</th>
+                            <th className={j.data}>Владелец</th>
+                            <th className={j.data}>Кол-во побед</th>
+                        </tr>
+
+                        {resList && resList.map(i =>
                             <tr className={j.row}>
-                                <td className={j.data}>{i.id}</td>
-                                <td className={j.data}>{i.name}</td>
-                                <td className={j.data}>{i.suit}</td>
-                                <td className={j.data}>{i.age}</td>
-                                <td className={j.data}>{i.owner}</td>
-                                <td className={j.data}>{i.wins}</td>
+                                <td className={j.data}>{i['horse_id']}</td>
+                                <td className={j.data}>{i['horse_name']}</td>
+                                <td className={j.data}>{i['suit']}</td>
+                                <td className={j.data}>{i['horse_age']}</td>
+                                <td className={j.data}>{i['owner_id']}</td>
+                                <td className={j.data}>{i['horse_wins']}</td>
                             </tr>
 
                         )}
-                    
-                </table>
+
+                    </table>
                 </div>
             </div>
 
--- horse_racing_back/controllers/horses.js
+++ horse_racing_back/controllers/horses.js
@@ -20,26 +20,15 @@ exports.find_horses = app.get("", async(req, res) => {
         const sort = req.query.order;
         if (sort == '') {
             const Horse = await pool.query(
-                `SELECT "winners".horse_id, horse_name, suit, horse_age, owner_name, "wins" FROM (
-                    SELECT horse_id, COUNT(*) AS "wins" FROM winners
-                    JOIN participants ON winners.participant_id = participants.participant_id
-                    GROUP BY horse_id
-                    ) AS "winners"
-                    JOIN horses ON horses.horse_id = "winners".horse_id
-                    JOIN owners ON horses.owner_id = owners.owner_id
-                    ORDER BY horse_name ASC`
+                `SELECT horse_id, horse_name, suit, horse_age, owner_name, horse_wins FROM horses
+                JOIN owners ON owners.owner_id = horses.owner_id`
             )
             result = Horse["rows"]
         } else {
             const Horse = await pool.query(
-                `SELECT "winners".horse_id, horse_name, suit, horse_age, owner_name, "wins" FROM (
-                    SELECT horse_id, COUNT(*) AS "wins" FROM winners
-                    JOIN participants ON winners.participant_id = participants.participant_id
-                    GROUP BY horse_id
-                    ) AS "winners"
-                    JOIN horses ON horses.horse_id = "winners".horse_id
-                    JOIN owners ON horses.owner_id = owners.owner_id
-                    ORDER BY wins ${sort}`
+                `SELECT horse_id, horse_name, suit, horse_age, owner_name, horse_wins FROM horses
+                JOIN owners ON owners.owner_id = horses.owner_id
+                ORDER BY horse_wins ${sort}`
             )
             result = Horse["rows"]
         }
@@ -58,3 +47,12 @@ JOIN winners ON winners.participant_id = participants.participant_id
 JOIN races ON races.race_id = participants.race_id
 JOIN owners ON owners.owner_id = horses.owner_id
 GROUP BY horses.horse_id*/
+/*
+`SELECT "winners".horse_id, horse_name, suit, horse_age, owner_name, "wins" FROM (
+                    SELECT horse_id, COUNT(*) AS "wins" FROM winners
+                    JOIN participants ON winners.participant_id = participants.participant_id
+                    GROUP BY horse_id
+                    ) AS "winners"
+                    JOIN horses ON horses.horse_id = "winners".horse_id
+                    JOIN owners ON horses.owner_id = owners.owner_id
+                    ORDER BY horse_name ASC` */
