29ae154e9803094c7a1952685cfbb0a468e4fa6a
stakiv <125588447+stakiv@users.noreply.github.com>
2024-06-07 17:36:22 +0300

    Update jockeys.js

--- horse_racing_back/controllers/jockeys.js
+++ horse_racing_back/controllers/jockeys.js
@@ -21,24 +21,42 @@ exports.find_jockeys = app.get("", async (req, res) => {
         const filt = req.query.filter;
         if (sort == '' && filt == '') {
             const Jockey = await pool.query(
+                `SELECT j.jockey_id, j.jockey_name, j.jockey_age, COALESCE(w.wins, 0) AS wins
+                    FROM jockeys j
+                    FULL OUTER JOIN (
+                        SELECT jockey_id, COUNT(*) AS "wins" FROM winners
+                        JOIN participants ON winners.participant_id = participants.participant_id
+                        GROUP BY jockey_id
+                    ) w ON j.jockey_id = w.jockey_id
+                    ORDER BY j.jockey_name ASC;`
+                    /*
                 `SELECT "winners".jockey_id, jockey_name, jockey_age, "wins" FROM (
                     SELECT jockey_id, COUNT(*) AS "wins" FROM winners
                     JOIN participants ON winners.participant_id = participants.participant_id
                     GROUP BY jockey_id
                     ) AS "winners"
                     JOIN jockeys ON jockeys.jockey_id = "winners".jockey_id
-                    ORDER BY jockey_name ASC`
+                    ORDER BY jockey_name ASC`*/
             )
             result = Jockey['rows']
         } else if (filt == '') {
             const Jockey = await pool.query(
+                `SELECT j.jockey_id, j.jockey_name, j.jockey_age, COALESCE(w.wins, 0) AS wins
+                    FROM jockeys j
+                    FULL OUTER JOIN (
+                        SELECT jockey_id, COUNT(*) AS "wins" FROM winners
+                        JOIN participants ON winners.participant_id = participants.participant_id
+                        GROUP BY jockey_id
+                    ) w ON j.jockey_id = w.jockey_id
+                    ORDER BY "wins" ${sort}, j.jockey_name ASC;`
+                /*
                 `SELECT "winners".jockey_id, jockey_name, jockey_age, "wins" FROM (
                     SELECT jockey_id, COUNT(*) AS "wins" FROM winners
                     JOIN participants ON winners.participant_id = participants.participant_id
                     GROUP BY jockey_id
                     ) AS "winners"
                     JOIN jockeys ON jockeys.jockey_id = "winners".jockey_id
-                    ORDER BY "wins" ${sort}`
+                    ORDER BY "wins" ${sort}`*/
             )
             result = Jockey['rows']
         } else if (sort == '') {
